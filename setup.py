7f642a4741d693cdfb110e9827d14c52a58b47a1
Louis Thibault <louist@ltpc.(none)>
2012-10-30 17:11:03 +0100

    Fixed beautifulsoup dependency to bs4, and removed useless import in __init__.py

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     author_email='',
     packages=['scrappy'],
     include_package_data=True,
-    install_requires=['guessit', 'requests', 'beautifulsoup'],
+    install_requires=['guessit', 'requests', 'beautifulsoup4'],
     url='https://github.com/louist87/scrappy',
     license='GPL 3.0',
     description='TVDB lookup and intelligent file renaming',
-    keywords=["XBMC", "TVDB", "thetvdb", "plugin", "rename"],
+    keywords=["TVDB", "thetvdb", "rename", "broadcatching", "media"],
     long_description=open('README.md').read()
 )
