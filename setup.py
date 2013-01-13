f591af074ec87693b69a26df8effb162bf1c1521
Louis Thibault <louist@ltpc.(none)>
2013-01-13 17:04:28 +0100

    version++

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Scrappy',
-    version="0.2.10 beta",
+    version="0.2.10 beta 2",
     author='Louis Thibault',
     author_email='louist87@gmail.com',
     packages=['scrappy'],
